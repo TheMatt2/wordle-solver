3016c785ce95c5bbba238f821300a316017a6ed0
TheMatt2 <22862051+TheMatt2@users.noreply.github.com>
2023-03-12 17:53:07 -0400

    Add stats to benchmark

--- wordle_benchmark.py
+++ wordle_benchmark.py
@@ -2,7 +2,9 @@
 For a given solution set, benchmark all solutions and see
 how many turns on average are taken to solve.
 """
+import time
 import random
+from collections import Counter
 
 import wordle_main
 import wordle_test
@@ -28,20 +30,35 @@ def main():
     if naive:
         solutions = word_list
 
+    random.seed(12345)
     random.shuffle(solutions)
 
     with open("word_results.txt", "w") as f:
-        turns_count = 0
-        turns_total = 0
+        start = time.perf_counter()
+        turn_count = 0
+        turn_total = 0
+        turn_stats = Counter()
+
         for solution in progress_bar(solutions):
             turns = wordle_test.test_wordle(context, naive, solution, progress = False)
-            turns_total += turns
-            turns_count += 1
+            turn_count += 1
+            turn_total += turns
+            turn_stats[turns] += 1
 
-            print(f"{solution} solved in {turns} Turns average: {turns_total / turns_count:.2f}",
-                  file = f, flush = True)
+            print(f"{solution} solved in {turns} Turns average: {turn_total / turn_count:.2f}",
+                file = f, flush = True)
 
-    print(f"Solved in an average of {turns_total / turns_count:.2f} turns.")
+        stop = time.perf_counter()
+
+        # Print both to file and stdout
+        for fd in [f, None]:
+            print(file = fd)
+            for i in range(min(turn_stats.keys()), max(turn_stats.keys()) + 1):
+                print(f"Solved {turn_stats[i]} words in {i} turns", file = fd)
+
+            print(f"Solved words an average of {turn_total / turn_count:.2f} "
+                f"turns in {stop - start:.2f} secs",
+                file = fd)
 
 if __name__ == "__main__":
     main()
